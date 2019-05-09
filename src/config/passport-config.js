2a3cd599510b5da9224492036952c6f3346f5198
AntiZERO <gray2884@gmail.com>
2019-05-09 17:14:53 -0500

    first commit -- server listening successfully

---
+++ src/config/passport-config.js
@@ -0,0 +1,8 @@
+// #1 - Import Passport  and the local strategy to use in our implementation. Import User model and helper module. 
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+
+module.exports = {
+ 
+}
---
+++ src/controllers/staticController.js
@@ -0,0 +1,10 @@
+/*Will define and export an object that contains multiple functions. 
+Each function will contain a handler for a particular route.*/
+
+module.exports = {
+
+  index(req, res, next) {
+    res.render("static/index", {title: "Welcome to Blocipedia"});
+  },
+
+}
