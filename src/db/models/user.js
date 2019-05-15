e05b7e3d5f5e7172f11caedb2cfac4f2eb0b950f
AntiZERO <gray2884@gmail.com>
2019-05-15 17:55:26 -0500

    adding reminder as note in models/user.js

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
     role: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: 0
+      defaultValue: 0       // 0 (Free) / 1 (Premium)
     },
     email: {
       type: DataTypes.STRING,
